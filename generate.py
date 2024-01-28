f42dac87d9dae569309c000e7f244cfee6d95f61
tcarlucc <tcarlucc@uvm.edu>
2024-01-28 11:01:23 -0500

    generate.py creates a 5x10x5 series of boxes, decaying in size as height increases. Stored in boxes.sdf, which is now used in simulate.py.

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+for i in range(10):
+    for j in range(5):
+        for k in range(5):
+            s = 1 if i == 0 else 0.9 ** i  # Decay size of box 10% each iteration
+            pyrosim.Send_Cube(name="Box", pos=[j, k, (.5 + i)], size=[s, s, s])
+
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -6,9 +6,10 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
+
 p.disconnect()
