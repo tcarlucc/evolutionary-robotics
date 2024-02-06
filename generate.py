1eea0ff15b30acaf1e4a4ad78af7d116a586ce92
tcarlucc <tcarlucc@uvm.edu>
2024-02-05 20:59:03 -0500

    generate.py: World creation now located in Create_World() function.
    simulate.py: Uses new world.sdf from gen.py

--- generate.py
+++ generate.py
@@ -1,12 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0], size=[1, 1, 1])
+    pyrosim.End()
 
-for i in range(10):
-    for j in range(5):
-        for k in range(5):
-            s = 1 if i == 0 else 0.9 ** i  # Decay size of box 10% each iteration
-            pyrosim.Send_Cube(name="Box", pos=[j, k, (.5 + i)], size=[s, s, s])
-
-pyrosim.End()
-
+Create_World()
