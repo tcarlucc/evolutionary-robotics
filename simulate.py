5e87667cce6b81b1ddefe6244acef9a23701a888
tcarlucc <tcarlucc@uvm.edu>
2024-02-12 21:48:45 -0500

    First 'sensors' commit. simulate.py now utilizes pyrosim for sensor integration.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -8,8 +9,11 @@ p.setGravity(0, 0, -9.8)
 robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+
 for i in range(10000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
     print(i)
 
