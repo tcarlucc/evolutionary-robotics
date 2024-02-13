63af908374b78399d0dacd765c1a2b0e2976db7c
tcarlucc <tcarlucc@uvm.edu>
2024-02-12 22:05:52 -0500

    End of 'storing sensor values.' Sensor output now stored in numpy vector.

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -11,13 +12,14 @@ planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-for i in range(10000):
+backLegSensorValues = numpy.zeros(10000)
+for i in range(1000):
     p.stepSimulation()
 
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 
     time.sleep(1/60)
     # print(i)   Removed for the sensor statement above
 
 p.disconnect()
+print(backLegSensorValues)
