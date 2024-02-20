09ae3d5dab85e7441a72ac6fe43fbfb33105d074
tcarlucc <tcarlucc@uvm.edu>
2024-02-19 20:59:24 -0500

    End of "Adding a Motor". Joint that links back leg and torso now resists movement. (Note, line 25 jointName parameter needs a bytestring on my machine).

--- simulate.py
+++ simulate.py
@@ -22,6 +22,8 @@ for i in range(ITERATIONS):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_BackLeg", controlMode=p.POSITION_CONTROL, targetPosition=0.0, maxForce=500)
+
     time.sleep(1/60)
     # print(i)   Removed for the sensor statement above
 
