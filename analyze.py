e282f4779da8d0eee5ea7edb2a81da043161924f
tcarlucc <tcarlucc@uvm.edu>
2024-02-19 21:42:40 -0500

    End of "open loop control of movement" module. Robot now utilizes a sinusoidal pattern of oscillating motor values to consistently walk in one direction.

--- analyze.py
+++ analyze.py
@@ -3,11 +3,20 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+targetAngles = np.load("data/targetAngles.npy")
 
-plt.plot(backLegSensorValues, label="Back Leg", linewidth=2.0)
-plt.plot(frontLegSensorValues, label="Front Leg")
-plt.xlabel("Time")
-plt.ylabel("Sensor Value")
-plt.legend()
+def plot_sensor_values():
+    plt.plot(backLegSensorValues, label="Back Leg", linewidth=2.0)
+    plt.plot(frontLegSensorValues, label="Front Leg")
+    plt.xlabel("Time")
+    plt.ylabel("Sensor Value")
+    plt.legend()
+    plt.show()
 
-plt.show()
+def plot_target_angles():
+    plt.plot(targetAngles)
+    plt.xlabel("Time")
+    plt.ylabel("Target Angle")
+    plt.show()
+
+plot_target_angles()
--- simulate.py
+++ simulate.py
@@ -7,6 +7,14 @@ import random
 
 ITERATIONS = 1000
 
+front_leg_amplitude = np.pi/3
+front_leg_frequency = 20
+front_leg_phaseOffset = 0
+
+back_leg_amplitude = np.pi/4
+back_leg_frequency = 10
+back_leg_phaseOffset = np.pi/4
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
@@ -17,6 +25,12 @@ pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = np.zeros(ITERATIONS)
 frontLegSensorValues = np.zeros(ITERATIONS)
+
+back_leg_target_angles = back_leg_amplitude * np.sin(back_leg_frequency * np.linspace(0, 2*np.pi, ITERATIONS) +
+                                                     front_leg_phaseOffset)
+front_leg_target_angles = front_leg_amplitude * np.sin(front_leg_frequency * np.linspace(0, 2*np.pi, ITERATIONS) +
+                                                       back_leg_phaseOffset)
+
 for i in range(ITERATIONS):
     p.stepSimulation()
 
@@ -24,14 +38,14 @@ for i in range(ITERATIONS):
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_BackLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=(random.random()-0.5)*np.pi, maxForce=50)
+                                targetPosition=back_leg_target_angles[i], maxForce=50)
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b"Torso_FrontLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=(random.random()-0.5)*np.pi, maxForce=50)
+                                targetPosition=front_leg_target_angles[i], maxForce=50)
 
     time.sleep(1/60)
     # print(i)   Removed for the sensor statement above
 
 np.save("data/backLegSensorValues.npy", backLegSensorValues)
 np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
+np.save("data/targetAngles.npy", targetAngles)
 p.disconnect()
-print(backLegSensorValues)
