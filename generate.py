ad732812e3f722d813e9cb3e8603849556d9f540
tcarlucc <tcarlucc@uvm.edu>
2024-03-04 20:42:17 -0500

    End of first module. Generate.py now creates a nndf file for the 'brain' of the robot.

--- generate.py
+++ generate.py
@@ -1,20 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-
-def Create_World():
-    pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5], size=[1, 1, 1])
-    pyrosim.End()
-
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", position=[0.5, 0, 1], type="revolute")
     pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", position=[-0.5, 0, 1], type="revolute")
     pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
-
     pyrosim.End()
 
-Create_World()
-Create_Robot()
+
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
+    pyrosim.End()
+
+Generate_Body()
+Generate_Brain()
