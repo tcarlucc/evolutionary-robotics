222556259be146c086ad83c2473fee25f0be32ce
tcarlucc <tcarlucc@uvm.edu>
2024-03-25 09:50:35 -0400

    End of 'evaluate' module. Simulation process now modularized for automation. Next step is determining fitness.

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ import os
 from hillclimber import HILL_CLIMBER
 
 hc = HILL_CLIMBER()
-
+hc.Evolve()
 
 """for i in range(5):
     os.system("py generate.py")
--- solution.py
+++ solution.py
@@ -1,5 +1,44 @@
 import numpy as np
+import pyrosim.pyrosim as pyrosim
+import os
 
 class SOLUTION:
     def __init__(self):
         self.weights = (np.random.rand(3, 2) * 2) - 1
+
+    def Evaluate(self):
+        self.Create_World()
+        self.Generate_Body()
+        self.Generate_Brain()
+        os.system('py simulate.py')
+
+    def Create_World(self):
+        pyrosim.Start_SDF("world.sdf")
+        pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5], size=[1, 1, 1])
+        pyrosim.End()
+
+    def Generate_Body(self):
+        pyrosim.Start_URDF("body.urdf")
+        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[1, 1, 1])
+        pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", position=[0.5, 0, 1],
+                           type="revolute")
+        pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", position=[-0.5, 0, 1],
+                           type="revolute")
+        pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+        pyrosim.End()
+
+    def Generate_Brain(self):
+        pyrosim.Start_NeuralNetwork("brain.nndf")
+        pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
+        pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
+        pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
+        pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
+
+        for currentRow in range(0, 3):
+            for currentColumn in range(0, 2):
+                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+3,
+                                     weight=self.weights[currentRow][currentColumn])
+
+        pyrosim.End()
