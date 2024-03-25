5ccc80c44ac12f918c9a2dd50c50d8fb2eb43952
tcarlucc <tcarlucc@uvm.edu>
2024-03-25 10:12:10 -0400

    End of 'evaluate for one generation' module. Hill climber now runs for one generation and determines fitness of both parent and child.

---
+++ hillclimber.py
@@ -0,0 +1,33 @@
+from solution import SOLUTION
+from constants import numberOfGenerations
+import copy
+
+
+class HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()
+
+    def Evolve(self):
+        self.parent.Evaluate()
+        for currentGeneration in range(numberOfGenerations):
+            self.Evolve_For_One_Generation()
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate()
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent = self.child
+
+    def Print(self):
+        print(self.parent.fitness, self.child.fitness)
