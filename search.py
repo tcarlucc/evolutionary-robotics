7f73cfb095353d8618300af186e706074e07472a
tcarlucc <tcarlucc@uvm.edu>
2024-03-25 09:38:48 -0400

    Started work on the hill climber by adding a dedicated class for it and a solution class. Solution will store the optimal synaptic weights.

---
+++ search.py
@@ -0,0 +1,9 @@
+import os
+from hillclimber import HILL_CLIMBER
+
+hc = HILL_CLIMBER()
+
+
+"""for i in range(5):
+    os.system("py generate.py")
+    os.system("py simulate.py")"""
---
+++ solution.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class SOLUTION:
+    def __init__(self):
+        self.weights = (np.random.rand(3, 2) * 2) - 1
